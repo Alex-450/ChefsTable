0f939c200a94920520be1ae57f1c64d94e838164
Victoria Shanukova <vshanukova@gmail.com>
2021-08-23 14:22:01 +0100

    done routes and controller for chef_profile

---
+++ app/controllers/chef_profiles_controller.rb
@@ -0,0 +1,33 @@
+class ChefProfilesController < ApplicationController
+  before_action :set_chef_profile, only: :show
+
+  def index
+    @chef_profiles = ChefProfile.all
+  end
+
+  def show
+  end
+
+  def new
+    @chef_profile = ChefProfile.new
+  end
+
+  def create
+    @chef_profile = ChefProfile.create(chef_profile_params)
+    if chef_profile.save
+      redirect_to chef_profiles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_chef_profile
+    @chef_profile = ChefProfile.find(params[:id])
+  end
+
+  def chef_profile_params
+    params.require(:chef_profile).permit(:user_id)
+  end
+end
