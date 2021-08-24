93c7080cdff68cbc2d889d439c5a65229c406d1c
mick butler <mickbutler@gmail.com>
2021-08-24 10:04:44 +0200

    added typed to the banner

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,7 @@ const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Good food is a celebration for life.."],
     typeSpeed: 50,
-    loop: true
+    loop: false
   });
 }
 
