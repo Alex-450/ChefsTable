cd2d961a383ec00589b5b97752f1153f1d795cfb
Victoria Shanukova <vshanukova@gmail.com>
2021-08-25 11:23:30 +0100

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_075721) do
+ActiveRecord::Schema.define(version: 2021_08_25_085159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2021_08_25_075721) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "cuisine"
     t.string "location"
+    t.text "cuisines", default: [], array: true
     t.index ["user_id"], name: "index_chef_profiles_on_user_id"
   end
 
